ba4e4a5a6b0886cdde62fffac0332623c155336d
Denes Toth <Denes_Toth@epam.com>
2017-03-28 16:09:29 +0200

    add first controller

--- main/src/main/java/com/dnstth/vtmg/Application.java
+++ main/src/main/java/com/dnstth/vtmg/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainClass {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainClass.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ main/src/main/java/com/dnstth/vtmg/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.dnstth.vtmg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Denes_Toth
+ */
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String getIndex(){
+        return "index";
+    }
+
+}
