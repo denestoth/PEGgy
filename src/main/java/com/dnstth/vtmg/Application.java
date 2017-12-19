bc67119e69adda9abe72ec2cbe17aa791afda607
Denes Toth <v-detoth@hotels.com>
2017-12-19 17:44:55 +0100

    put stuff back into their place

--- src/main/java/com/dnstth/vtmg/Application.java
+++ src/main/java/com/dnstth/vtmg/Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
 @ComponentScan({"com.dnstth.vtmg"})
 @EntityScan({"com.dnstth.vtmg.dal.dto"})
 @EnableJpaRepositories
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
